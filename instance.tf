2e3aebd03b86809d1d52d40a5af52e42ce8a1f59
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-29 12:09:38 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "terraform" {
+  project      = var.project
+  name         = var.instance-name
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
